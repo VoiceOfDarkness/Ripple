5c42cd409b6f2d5c72cc964ee7f35d0e278d5ce3
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-05-21 19:32:25 +0400

    config file dark mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  // darkMode: 'selector',
   theme: {
     colors: {
       cardBlack: "#0D0D0D",
