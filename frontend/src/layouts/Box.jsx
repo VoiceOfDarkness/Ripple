923dadc9daf81b5652f0538f7299c4efd1054995
ravan-Aliyev <132194049+ravan-Aliyev@users.noreply.github.com>
2024-05-30 13:23:19 +0400

    fix: spacing problems

--- frontend/src/layouts/Box.jsx
+++ frontend/src/layouts/Box.jsx
@@ -1,7 +1,7 @@
 export default function Box({ children, className, style }) {
   return (
     <div
-      className={`w-128 p-14 m-auto max-lg:w-11/12 ${className || ""}`}
+      className={`w-128 m-auto max-lg:w-11/12 ${className || ""}`}
       style={style}
     >
       {children}
