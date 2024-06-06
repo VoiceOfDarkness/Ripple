e1ce7065f8cc278251849982ac0cf941cef90396
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-06-07 00:02:59 +0400

    feat: JobDetails page created -v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+// App.js
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import AuthPage from "./pages/Auth";
 import JobMainLayout from "./layouts/JobMainLayaout";
 import JobMain from "./components/JobMain";
 import Profile from "./pages/Profile";
+import JobDetails from "./components/JobDetails"; // Import the JobDetails component
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Route element={<JobMainLayout />}>
         <Route path="/jobs" element={<JobMain />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/job/:id" element={<JobDetails />} /> {/* Add the job details route */}
       </Route>
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/dashboard" element={<AuthPage />} />
--- frontend/src/components/JobCards/JobCard.jsx
+++ frontend/src/components/JobCards/JobCard.jsx
@@ -1,4 +1,6 @@
+// JobCard.js
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 
@@ -9,17 +11,24 @@ const InfoTag = ({ children }) => {
     </span>
   );
 };
+
 export default function JobCard({ job }) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const navigate = useNavigate();
 
-  const handleToggle = () => {
+  const handleToggle = (event) => {
+    event.stopPropagation(); // Prevent triggering the handleClick
     setIsExpanded(!isExpanded);
+  };
+
+  const handleClick = () => {
+    navigate(`/job/${job.id}`);
   };
 
   return (
     <div
-      className={`bg-black w-full shadow-md p-6 rounded-3xl 
-      }`}>
+      className="bg-black w-full shadow-md p-6 rounded-3xl cursor-pointer"
+      onClick={handleClick}>
       <div className="flex w-full justify-between content-between">
         <div className="w-16 h-16 flex items-center justify-center bg-inputGray rounded-full"></div>
         {isExpanded ? (
@@ -31,14 +40,14 @@ export default function JobCard({ job }) {
           />
         )}
       </div>
-      <h1 className="text-white font-bold  mt-5">{job.title}</h1>
+      <h1 className="text-white font-bold mt-5">{job.title}</h1>
       <h2 className="text-white">{job.company}</h2>
       <div className="flex gap-3 mt-10">
         <InfoTag>{job.location}</InfoTag>
         <InfoTag>{job.postedAt}</InfoTag>
         <InfoTag>{job.contract}</InfoTag>
       </div>
-      <div className="flex  mt-10 gap-10">
+      <div className="flex mt-10 gap-10">
         {job.isNew && (
           <span className="bg-red text-white py-1 px-3 rounded-3xl">New</span>
         )}
